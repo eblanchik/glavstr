b71a96ace3437d66bd98df38e3bddbb650310399
eblanchik <72874091+eblanchik@users.noreply.github.com>
2020-11-16 07:16:59 +0200

    говноо

---
+++ script.js
@@ -0,0 +1,9 @@
+function gachi(){
+    document.location.href = "https://eblanchik.github.io/gachimuchi.github.io/";
+}
+function reih(){
+    document.location.href = "https://eblanchik.github.io/3reih/";
+}
+function codds(){
+    alert("'СССР'\n 'Дискриминант'\n 'Аниме' \n'удалить'\n Остальные коды остануться тайной...");
+}
